a925a4debbb3229ba6b8c1fd9c78990beae19f6f
iigmir <roc120j@gmail.com>
2019-06-28 10:34:46 +0800

    Linting

--- .eslintrc.js
+++ .eslintrc.js
@@ -9,7 +9,13 @@ module.exports = {
   ],
   rules: {
     'no-console': process.env.NODE_ENV === 'production' ? 'error' : 'off',
-    'no-debugger': process.env.NODE_ENV === 'production' ? 'error' : 'off'
+    'no-debugger': process.env.NODE_ENV === 'production' ? 'error' : 'off',
+    "semi": ["error", "always"],
+    "quotes": ["error", "double", {
+        "avoidEscape": true,
+        "allowTemplateLiterals": true
+    }],
+    "indent": ["error", 4]
   },
   parserOptions: {
     parser: 'babel-eslint'
