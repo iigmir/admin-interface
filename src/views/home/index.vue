ef2406caa52c17bccbb853d91b3eb8a602818052
iigmir <roc120j@gmail.com>
2019-06-28 10:58:50 +0800

    Build my own architecture

--- src/views/home/index.vue
+++ src/views/home/index.vue
@@ -1,9 +1,9 @@
 <template>
-  <HelloWorld />
+    <HelloWorld />
 </template>
 
 <script>
-import HelloWorld from "../components/HelloWorld";
+import HelloWorld from "./components/HelloWorld.vue";
 
 export default {
     components: {
