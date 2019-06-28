829b1c32578090754b121b5674f5d6a824cc73c8
iigmir <roc120j@gmail.com>
2019-06-29 01:51:27 +0800

    Write case

--- tests/unit/user.spec.js
+++ tests/unit/user.spec.js
@@ -1,10 +1,45 @@
 import { expect } from "chai";
-import { shallowMount } from "@vue/test-utils";
+import Vuex from "vuex";
+import { shallowMount, createLocalVue } from "@vue/test-utils";
 import User from "../../src/views/users/index.vue";
 
-describe("User.vue", () => {
-    it("renders props.msg when passed", () => {
-        const wrapper = shallowMount(User);
-        expect( wrapper.grouped_list.length ).to.equal(4);
+const localVue = createLocalVue();
+
+localVue.use(Vuex);
+
+describe("User.vue", () =>
+{
+    let store;
+    beforeEach(() =>
+    {
+        store = new Vuex.Store({
+            modules: {
+                users: {
+                    state: {
+                        list: [
+                            {},{},{},{},{},
+                            {},{},{},{},{},
+                            {},{},{},{},{},
+                            {},{},{},{},{}
+                        ]
+                    },
+                    actions: {
+                        FETCH_list: () => {}
+                    }
+                }
+            }
+        });
+    });
+    it("grouped_list should be correct sorted", () =>
+    {
+        const wrapper = shallowMount(User, { store, localVue });
+        const grouped_list = wrapper.vm.grouped_list;
+        const correct_case = [
+            [{},{},{},{},{}],
+            [{},{},{},{},{}],
+            [{},{},{},{},{}],
+            [{},{},{},{},{}]
+        ];
+        expect( grouped_list ).to.deep.equal( correct_case );
     });
 });
